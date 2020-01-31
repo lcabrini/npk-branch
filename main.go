d137b9a8d2d730f7234f698b78dec3c1f364de29
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2020-01-31 14:39:25 +0000

    Exit if unable to connect to database on startup.

--- main.go
+++ main.go
@@ -2,13 +2,21 @@ package main
 
 import (
     "fmt"
+    "os"
+    "log"
     "net/http"
     "github.com/gorilla/mux"
     "github.com/lcabrini/npk-common"
 )
 
 func main() {
-    npk.DBConnection(config)
+    if _, err := npk.DBConnection(config); err != nil {
+        log.Printf("database: %v", err)
+        os.Exit(1)
+    } else {
+        log.Printf("connected to database")
+    }
+
     npk.SetupSessionStore(config)
     r := mux.NewRouter()
     //r.HandleFunc("/", RootHandler)
